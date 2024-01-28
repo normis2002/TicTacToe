7426f98282bf4d1f8c11f98f192826b583d526e7
Artur <arturovitulo@gmail.com>
2024-01-28 17:44:42 +0200

    add draw and name winner

--- main.js
+++ main.js
@@ -1,51 +1,63 @@
 let cells = document.querySelectorAll('#area td');
+
 function start(cells) {
-	let i = 0;
+    let i = 0;
+    let cellsNow;
 
-	for (let cell of cells) {
-		cell.addEventListener('click', function () {
-			if (!this.textContent) {
-				if (i % 2 == 0) {
-					this.textContent = 'X';
-				} else {
-					this.textContent = '0';
-				}
+    function isVictory(cells) {
+        let combs = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
 
-				i++;
-			}
+        for (let comb of combs) {
+            if (
+                cells[comb[0]].textContent == cells[comb[1]].textContent &&
+                cells[comb[1]].textContent == cells[comb[2]].textContent &&
+                cells[comb[0]].textContent != ''
+            ) {
+                return cellsNow;
+            }
+        }
+        return false;
+    }
 
-			function isVictory(cells) {
-				let combs = [
-					[0, 1, 2],
-					[3, 4, 5],
-					[6, 7, 8],
-					[0, 3, 6],
-					[1, 4, 7],
-					[2, 5, 8],
-					[0, 4, 8],
-					[2, 4, 6],
-				];
+    function areAllCellsFilled(cells) {
+        for (let cell of cells) {
+            if (!cell.textContent) {
+                return false; // Принаймні одна комірка не заповнена
+            }
+        }
+        return true; // Всі комірки заповнені
+    }
 
-				for (let comb of combs) {
-					if (
-						cells[comb[0]].textContent == cells[comb[1]].textContent &&
-						cells[comb[1]].textContent == cells[comb[2]].textContent &&
-						cells[comb[0]].textContent != ''
-					) {
-						return true;
-					}
-				}
+    for (let cell of cells) {
+        cell.addEventListener('click', function () {
+            if (!this.textContent) {
+                if (i % 2 == 0) {
+                    cellsNow = this.textContent = 'X';
+                } else {
+                    cellsNow = this.textContent = '0';
+                }
 
-				return false;
-			}
-			let winner = isVictory(cells);
-			if (winner) {
-				console.log(`Winner: ${winner}`);
-			} else {
-				console.log("It's a draw!");
-			}
+                i++;
+            }
 
-		});
-	}
+            let winner = isVictory(cells);
+
+            if (winner) {
+                console.log(`Winner: ${winner}`);
+            } else if (areAllCellsFilled(cells)) {
+                console.log("It's a draw!");
+            }
+        });
+    }
 }
+
 start(cells);
